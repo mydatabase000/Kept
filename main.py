bf99b007cb59da83f01015f69b1b3b17909668b9
klept0 <klept0iz@gmail.com>
2023-10-24 20:59:55 -0400

    Testing new wait times
    Making things more human like when searching - increased random wait times

--- main.py
+++ main.py
@@ -1,29 +1,77 @@
 import argparse
+import csv
 import json
 import logging
 import logging.handlers as handlers
 import random
 import sys
+import time
 from pathlib import Path
+import pandas as pd
+from datetime import datetime
 
 from src import Browser, DailySet, Login, MorePromotions, PunchCards, Searches
 from src.loggingColoredFormatter import ColoredFormatter
 from src.notifier import Notifier
+from src.utils import Utils
 
 POINTS_COUNTER = 0
 
-
 def main():
+    print("test", Utils.randomSeconds(5, 10))
     args = argumentParser()
     notifier = Notifier(args)
     setupLogging(args.verbosenotifs, notifier)
     loadedAccounts = setupAccounts()
+
+    # Load previous day's points data
+    previous_points_data = load_previous_points_data()
+
     for currentAccount in loadedAccounts:
         try:
-            executeBot(currentAccount, notifier, args)
+            earned_points = executeBot(currentAccount, notifier, args)
+            account_name = currentAccount.get("username", "")
+            previous_points = previous_points_data.get(account_name, 0)
+
+            # Calculate the difference in points from the prior day
+            points_difference = earned_points - previous_points
+
+            # Append the daily points and points difference to CSV and Excel
+            log_daily_points_to_csv(account_name, earned_points, points_difference)
+
+            # Update the previous day's points data
+            previous_points_data[account_name] = earned_points
+
+            print(f"[POINTS] data for '{account_name}' appended to the file.")
         except Exception as e:
             logging.exception(f"{e.__class__.__name__}: {e}")
 
+    # Save the current day's points data for the next day in the "logs" folder
+    save_previous_points_data(previous_points_data)
+    print("Points data saved for the next day.")
+
+def log_daily_points_to_csv(date, earned_points, points_difference):
+    logs_directory = Path(__file__).resolve().parent / "logs"
+    csv_filename = logs_directory / "points_data.csv"
+
+    # Create a new row with the date, daily points, and points difference
+    date = datetime.now().strftime("%Y-%m-%d")
+    new_row = {
+        "Date": date,
+        "Earned Points": earned_points,
+        "Points Difference": points_difference,
+    }
+
+    fieldnames = ["Date", "Earned Points", "Points Difference"]
+    is_new_file = not csv_filename.exists()
+
+    with open(csv_filename, mode="a", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        
+        if is_new_file:
+            writer.writeheader()
+        
+        writer.writerow(new_row)
 
 def setupLogging(verbose_notifs, notifier):
     ColoredFormatter.verbose_notifs = verbose_notifs
@@ -33,14 +81,15 @@ def setupLogging(verbose_notifs, notifier):
     terminalHandler = logging.StreamHandler(sys.stdout)
     terminalHandler.setFormatter(ColoredFormatter(format))
 
-    (Path(__file__).resolve().parent / "logs").mkdir(parents=True, exist_ok=True)
+    logs_directory = Path(__file__).resolve().parent / "logs"
+    logs_directory.mkdir(parents=True, exist_ok=True)
 
     logging.basicConfig(
         level=logging.INFO,
         format=format,
         handlers=[
             handlers.TimedRotatingFileHandler(
-                "logs/activity.log",
+                logs_directory / "activity.log",
                 when="midnight",
                 interval=1,
                 backupCount=2,
@@ -94,7 +143,7 @@ def argumentParser() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def setupAccounts() -> dict:
+def setupAccounts() -> list:
     accountPath = Path(__file__).resolve().parent / "accounts.json"
     if not accountPath.exists():
         accountPath.write_text(
@@ -131,10 +180,18 @@ def executeBot(currentAccount, notifier: Notifier, args: argparse.Namespace):
             remainingSearches,
             remainingSearchesM,
         ) = desktopBrowser.utils.getRemainingSearches()
+
+        # Introduce random pauses before and after searches
+        pause_before_search = random.uniform(1.0, 5.0)  # Random pause between 1 to 5 seconds
+        time.sleep(pause_before_search)
+
         if remainingSearches != 0:
             accountPointsCounter = Searches(desktopBrowser).bingSearches(
                 remainingSearches
             )
+
+        pause_after_search = random.uniform(1.0, 5.0)  # Random pause between 1 to 5 seconds
+        time.sleep(pause_after_search)
 
         if remainingSearchesM != 0:
             desktopBrowser.closeBrowser()
@@ -145,6 +202,7 @@ def executeBot(currentAccount, notifier: Notifier, args: argparse.Namespace):
                 accountPointsCounter = Searches(mobileBrowser).bingSearches(
                     remainingSearchesM
                 )
+
         desktopBrowser.closeBrowser()
 
         logging.info(
@@ -157,13 +215,48 @@ def executeBot(currentAccount, notifier: Notifier, args: argparse.Namespace):
         notifier.send(
             "\n".join(
                 [
-                    "MS Rewards Farmer",
-                    f"Account: {currentAccount.get('username', '')}",
+                    "🏅 MS Rewards Farmer",
+                    f"👤 Account: {currentAccount.get('username', '')}",
                     f"⭐️ Points earned today: {desktopBrowser.utils.formatNumber(accountPointsCounter - startingPoints)}",
-                    f"🏅 Total points: {desktopBrowser.utils.formatNumber(accountPointsCounter)}",
+                    f"💰 Total points: {desktopBrowser.utils.formatNumber(accountPointsCounter)}",
                 ]
             )
         )
+
+        return accountPointsCounter
+
+
+def export_points_to_csv(points_data):
+    logs_directory = Path(__file__).resolve().parent / "logs"
+    csv_filename = logs_directory / "points_data.csv"
+    with open(csv_filename, mode="a", newline="") as file:  # Use "a" mode for append
+        fieldnames = ["Account", "Earned Points", "Points Difference"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+        # Check if the file is empty, and if so, write the header row
+        if file.tell() == 0:
+            writer.writeheader()
+
+        for data in points_data:
+            writer.writerow(data)
+
+
+# Define a function to load the previous day's points data from a file in the "logs" folder
+def load_previous_points_data():
+    logs_directory = Path(__file__).resolve().parent / "logs"
+    try:
+        with open(logs_directory / "previous_points_data.json", "r") as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        return {}
+
+
+# Define a function to save the current day's points data for the next day in the "logs" folder
+def save_previous_points_data(data):
+    logs_directory = Path(__file__).resolve().parent / "logs"
+    with open(logs_directory / "previous_points_data.json", "w") as file:
+        json.dump(data, file, indent=4)
 
 
 if __name__ == "__main__":
